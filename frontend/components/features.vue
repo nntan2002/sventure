ad8e5dbad1bfd0206970be009302a2612813934c
Thien._.An <nntan2002@gmail.com>
2023-11-24 15:33:31 +0700

    update

---
+++ frontend/components/features.vue
@@ -0,0 +1,13 @@
+<template>
+    <section>
+        <div class="features">
+            <div class="container">
+                <b-row>
+                    <b-col>
+                        
+                    </b-col>
+                </b-row>
+            </div>
+        </div>
+    </section>
+</template>

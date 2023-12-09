1f87d19e439dc8ce774786eb5593a6e53f1189c4
Thien._.An <nntan2002@gmail.com>
2023-12-09 10:20:05 +0700

    update

--- cms/src/api/page-home/routes/page-home.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * page-home router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::page-home.page-home');
--- cms/src/api/page-home/services/page-home.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * page-home service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::page-home.page-home');

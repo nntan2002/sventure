2470fc0232380ca3945ba4ed8141586640625eeb
Thien._.An <nntan2002@gmail.com>
2024-01-17 11:24:34 +0700

    update

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Sventure - Tương lai của doanh nhân Việt' },
+      // { hid: 'description', name: 'description', content: 'Sventure - Tương lai của doanh nhân Việt' },
       { name: 'format-detection', content: 'telephone=no' },
       {
         hid: 'og:description',

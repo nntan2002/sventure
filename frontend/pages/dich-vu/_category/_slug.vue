855d0c0581850607e8774d336f8d2960d8458c24
Thien._.An <nntan2002@gmail.com>
2024-01-22 16:58:05 +0700

    update

--- frontend/pages/dich-vu/_category/_slug.vue
+++ frontend/pages/dich-vu/_category/_slug.vue
@@ -1,86 +1,183 @@
 <template>
     <div class="main">
-        <item-Banner title="Detail Service" />
+        <item-banner v-if="banner" :banner="banner" />
         <section class="section-padding">
             <b-container>
                 <div class="service">
                     <div class="share-media">
                         <div class="date">
-                            29/05/2023 11:24:28 AM
+                            {{ formatDate }}
                         </div>
                         <div class="share-icon">
-                            <a href="#">
+                            <a :href="'https://www.facebook.com/share.php?u=' + banner.urlShare">
                                 <img src="~/assets/images/facebook.jpg" alt="">
                             </a>
                             <a href="#">
                                 <img src="~/assets/images/instagram.jpg" alt="">
                             </a>
-                            <a href="#">
+                            <a :href="'https://www.linkedin.com/sharing/share-offsite/?url=' + banner.urlShare">
                                 <img src="~/assets/images/linkedIn.jpg" alt="">
                             </a>
                         </div>
                     </div>
-                    <div class="content-detail mt-4">
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum. Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum. Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum. Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum. Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum. Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum. Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum. Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum. Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum. Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Corporis sapiente ratione reprehenderit
-                            expedita illo. Adipisci, odio eius pariatur inventore repudiandae dolorum ut nesciunt beatae
-                            aspernatur, nemo neque obcaecati eos? Cum.
-                        </p>
+                    <div v-if="changed_post_content" class="content-detail mt-4" v-html="changed_post_content">
                     </div>
                 </div>
             </b-container>
         </section>
     </div>
 </template>
-  
 <script>
+
+const qs = require('qs')
 export default {
-    name: 'ServicesCategories',
+    name: '',
     layout: 'default',
+
+    async asyncData({ app, $axios, redirect, params }) {
+        const query = qs.stringify({
+            populate: {
+                Banner: {
+                    populate: '*',
+                },
+                Thumbnail: {
+                    populate: '*',
+                },
+                Seo: {
+                    populate: '*',
+                }
+            },
+            filters: {
+                Slug: params.slug,
+            },
+            publicationState: 'live',
+        })
+
+        const response = await fetch(
+            $axios.defaults.baseURL + '/api/services?' + query
+        )
+
+        if (response.status >= 200 && response.status <= 299) {
+            const post = await response.json()
+            if (!post.data.length) {
+                redirect('/404')
+            } else {
+                console.log(post)
+                return { post }
+            }
+        } else {
+            redirect('/404')
+        }
+    },
 
     data() {
         return {
             baseURL: this.$axios.defaults.baseURL,
-            banner: [],
+            postdetail: null,
+            postTitle: null,
+            dataNews: null,
+            banner: {
+                Image: null,
+                Title: null,
+            },
+
         }
+    },
+
+    head() {
+        return {
+            title: this.postData.Title + ' - Tin Tức' || '',
+            meta: [
+                {
+                    property: 'og:image',
+                    content: this.postShareImage
+                },
+                {
+                    hid: 'og:title',
+                    property: 'og:title',
+                    content: this.postData?.Seo?.title || '',
+                },
+                {
+                    hid: 'og:description',
+                    property: 'og:description',
+                    content: this.postData?.Seo?.description || '',
+                },
+                {
+                    hid: 'og:card',
+                    property: 'twitter:card',
+                    content: this.postShareImage
+                },
+                {
+                    hid: 'twitter:description',
+                    property: 'twitter:description',
+                    content: this.postData?.Seo?.description || '',
+                },
+            ],
+        }
+    },
+
+    computed: {
+        changed_post_content() {
+            try {
+                return this.post?.data[0]?.attributes?.Content
+                    .split('/uploads/')
+                    .join(`${this.baseURL}/uploads/`)
+            } catch (error) {
+                console.log(error)
+                return ''
+            }
+        },
+        postData() {
+            try {
+                return this.post ? this.post?.data[0]?.attributes : {}
+            } catch (error) {
+                console.log(error)
+                return {}
+            }
+        },
+        postShareImage() {
+            try {
+                const url = this.post?.data[0]?.attributes?.Thumbnail?.data
+                    ? this.post?.data[0]?.attributes?.Thumbnail?.data?.attributes?.url || ''
+                    : ''
+                return this.baseURL + url
+            } catch (error) {
+                console.log(error)
+                return ''
+            }
+        },
+
+        formatDate() {
+            try {
+                const dl = new Date(this.post?.data[0]?.attributes?.publishedAt)
+                const options = {
+                    year: 'numeric',
+                    month: 'numeric',
+                    day: 'numeric',
+                }
+
+                return dl.toLocaleDateString('en-GB', options)
+            } catch (error) {
+                console.log(error)
+                return ''
+            }
+
+        },
+    },
+    mounted() {
+
+        if (this.postData) {
+            try {
+                this.banner.Title = this.postData?.Title || ''
+                this.banner.Image = this.postData?.Banner?.Image || ''
+                this.banner.urlShare = window.location.href
+                console.log(this.postData)
+                this.banner = JSON.parse(JSON.stringify(this.banner))
+            } catch (error) { }
+        }
+
+    },
+    methods: {
     },
 }
 </script>
-  
--- frontend/pages/dich-vu/_category/index.vue
+++ frontend/pages/dich-vu/_category/index.vue
@@ -5,34 +5,30 @@
             <b-container>
                 <div class="service">
                     <b-row class="justify-content-center text-center">
-                        <b-col md="8">
+                        <b-col md="12">
                             <h2 class="title-section mb-4">
-                                <!-- {{ $t('test') }} -->
-                                List Services
+                                Sventure
                             </h2>
                             <div class="mb-4">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos voluptates
-                                placeat rem iusto velit animi reiciendis aperiam consectetur est maxime unde doloribus
-                                laboriosam, sit ipsum repellendus vitae ad delectus!
+                                {{ infor?.Description }}
                             </div>
                         </b-col>
                     </b-row>
                     <b-row>
-                        <b-col v-for="item in 6" :key="item" md="4" class="mb-4">
-                            <nuxt-link to="/dich-vu/abc/abc">
+                        <b-col v-for="item in serviceArr" :key="item.id" md="4" class="mb-4">
+                            <nuxt-link :to="'/dich-vu/' + infor?.Slug + '/' + item?.attributes.Slug" v-if="item">
                                 <div class="box-services">
                                     <div class="img-box">
-                                        <b-img src="~/assets/images/services1.jpg"></b-img>
-                                        <b-img src="~/assets/images/services2.jpg"></b-img>
+                                        <b-img :src="baseURL + item?.attributes.Thumbnail?.data?.attributes?.url"></b-img>
+                                        <b-img
+                                            :src="baseURL + item?.attributes.HoverThumbnail?.data?.attributes?.url"></b-img>
                                     </div>
                                     <div class="content-box">
                                         <h4 class="title my-4">
-                                            Service {{ item }}
+                                            {{ item.attributes.Title }}
                                         </h4>
                                         <p>
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic suscipit eos quae,
-                                            nesciunt beatae fuga a necessitatibus eius vitae culpa molestiae iusto atque
-                                            est, illum labore, dolor magnam et id.
+                                            {{ item.attributes.Description }}
                                         </p>
                                     </div>
                                 </div>
@@ -46,6 +42,7 @@
 </template>
   
 <script>
+const qs = require('qs')
 export default {
     name: 'ServicesCategories',
     layout: 'default',
@@ -53,11 +50,79 @@ export default {
     data() {
         return {
             baseURL: this.$axios.defaults.baseURL,
-            banner: null,
+            banner: {
+                Image: null,
+                Title: null,
+            },
+            infor: null,
+            serviceArr: null,
         }
     },
     mounted() {
-
+        console.log(this.$route.params.category)
+        const query = qs.stringify(
+            {
+                populate: '*',
+                publicationState: 'live',
+                filters: {
+                    category_service: {
+                        Slug: {
+                            $eq: this.$route.params.category,
+                        }
+                    },
+                },
+            },
+            {
+                encodeValuesOnly: true, // prettify url
+            }
+        )
+        const query1 = qs.stringify(
+            {
+                populate: {
+                    Banner: {
+                        populate: {
+                            Image: true,
+                        },
+                    },
+                },
+                publicationState: 'live',
+                filters: {
+                    Slug: {
+                        $eq: this.$route.params.category,
+                    }
+                },
+            },
+            {
+                encodeValuesOnly: true, // prettify url
+            }
+        )
+        this.getService(query)
+        this.getCategoryService(query1)
+    },
+    methods: {
+        getService(params) {
+            this.$api
+                .getService(params)
+                .then((data) => {
+                    this.serviceArr = data.data ? data.data : ''
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        },
+        getCategoryService(params) {
+            this.$api
+                .getCategoryService(params)
+                .then((data) => {
+                    this.banner.Image = data.data ? data.data[0].attributes.Banner.Image : ''
+                    this.banner.Title = data.data ? data.data[0].attributes.Title : ''
+                    this.infor = data.data[0]?.attributes || null
+                    console.log(this.infor)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        },
     }
 }
 </script>
